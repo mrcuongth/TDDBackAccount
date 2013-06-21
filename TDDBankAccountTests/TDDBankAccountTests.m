72152a85f8b5aa2fdd94d4561bee34f0b00db660
Trinh Huy Cuong <mr.cuongth@gmail.com>
2013-06-21 14:06:44 +0700

    New test with Deposit amount to bank account

--- TDDBankAccountTests/TDDBankAccountTests.m
+++ TDDBankAccountTests/TDDBankAccountTests.m
@@ -20,7 +20,6 @@ describe(@"Test Back Account class", ^{
     });
     
     it(@"2. Tài khoản có tính persistent.", ^{
-        
         NSString *accountNumber = @"123";
         BankAccount *ba = [[BankAccount alloc] init];
         ba.accountNumber = accountNumber;
@@ -30,6 +29,22 @@ describe(@"Test Back Account class", ^{
         
         BankAccount *baGotFromDBWithAccountNumber = [BankAccount getAccount:accountNumber];
         [[baGotFromDBWithAccountNumber.accountNumber should] equal:accountNumber];
+    });
+    
+    it(@"3. BankAccount.deposit(accountNumber, amount, description) gửi thêm tiền vào tài khoản.", ^{
+        NSString *accountNumber = @"123";
+        NSNumber *depositAmount = @100;
+        NSString *description = @"Deposit amount of money to account number 123";
+        
+        BankAccount *baOriginal = [BankAccount getAccount:accountNumber];
+        float originalAmount = baOriginal.balance;
+        
+        [BankAccount deposit:accountNumber withAmount:depositAmount description:description];
+        
+        [[theValue(baOriginal.balance) should] equal:theValue(originalAmount + [depositAmount floatValue])];
+        
+        BankAccount *bankAccountAfterDeposit = [BankAccount getAccount:accountNumber];
+        [[theValue(bankAccountAfterDeposit.balance) should] equal:theValue(originalAmount + [depositAmount floatValue])];
     });
 });
 
